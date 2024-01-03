332e101dcce70a3f1c2bccd0b0095ae0443ccb39
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-01-04 03:07:50 +0530

    Add new images and update home page design

--- HTML/Multi-page/Real Estate/Real_state_static/src/components/Home.jsx
+++ HTML/Multi-page/Real Estate/Real_state_static/src/components/Home.jsx
@@ -1,15 +1,18 @@
 import Logo from '../assets/Icons/Logo.svg'
 // import Manrope from '../assets/Fonts/Manrope.ttf'
+import mainBackground from '../assets/Photos/mainBackground.png'
+
+
 
 const Home = () => {
 
   return (
     <div className=''>
-    <header className=' w-full bg-white flex flex-col justify-center items-center'>
-      <nav className='w-5/6 h-[15vh] flex justify-between space-x-4 border-b-2 border-blue-500 '>
+      <header className=' w-full h-[100vh] bg-white flex flex-col  items-center pt-2 bg-cover bg-center bg-no-repeat ' style={{ backgroundImage: `url(${mainBackground})` }}>
+      <nav className='w-5/6  flex justify-evenly space-x-20 border-b-2 border-blue-500 '>
         <img className=' size-20' src={Logo} alt="logo img" />
         {/* nav link */}
-        <ul className='w-full flex justify-evenly  items-center ml-20  '>
+        <ul className='w-2/4 flex justify-evenly  items-center ml-20 text-white '>
           <li><a href="#">Location</a></li>
           <li><a href="#">Blogs</a></li>
           <li><a href="#">Testimonial</a></li>
@@ -17,8 +20,8 @@ const Home = () => {
         </ul>
       </nav>
 
-      <div>
-        <h1>Home That Makes <br /> You Living Life!</h1>
+      <div className='w-2/3 flex-col justify-start mt-20 space-y-4 '>
+        <h1 className=' text-6xl font-bold'>Home That Makes <br /> You Living Life!</h1>
         <p>
           Are you looking for amazing Real estate? Don't worry we got it for you
         </p>
