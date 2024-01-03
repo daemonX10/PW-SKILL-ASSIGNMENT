2806cf51c58465b202d65df118045be70e4caa11
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-01-04 02:19:42 +0530

    Add React and Vite configuration files and dummy components

--- HTML/Multi-page/Real Estate/Real_state_static/src/components/Home.jsx
+++ HTML/Multi-page/Real Estate/Real_state_static/src/components/Home.jsx
@@ -1,14 +1,15 @@
-import { ReactComponent as Logo} from '../assets/Icons/Logo.svg'
+import Logo from '../assets/Icons/Logo.svg'
+// import Manrope from '../assets/Fonts/Manrope.ttf'
 
 const Home = () => {
 
   return (
-    <>
-    <header>
-      <nav>
-        <img src={Logo} alt="logo img" />
+    <div className=''>
+    <header className=' w-full bg-white flex flex-col justify-center items-center'>
+      <nav className='w-5/6 h-[15vh] flex justify-between space-x-4 border-b-2 border-blue-500 '>
+        <img className=' size-20' src={Logo} alt="logo img" />
         {/* nav link */}
-        <ul>
+        <ul className='w-full flex justify-evenly  items-center ml-20  '>
           <li><a href="#">Location</a></li>
           <li><a href="#">Blogs</a></li>
           <li><a href="#">Testimonial</a></li>
@@ -16,9 +17,15 @@ const Home = () => {
         </ul>
       </nav>
 
-
+      <div>
+        <h1>Home That Makes <br /> You Living Life!</h1>
+        <p>
+          Are you looking for amazing Real estate? Don't worry we got it for you
+        </p>
+        <button>Learn More</button>
+      </div>
     </header>
-    </>
+    </div>
   )
 }
 
